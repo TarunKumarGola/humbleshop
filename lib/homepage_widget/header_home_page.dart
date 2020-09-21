b9789588f4ef0def98e9605483d009874ea048e5
asaksham <asaksham999@gmail.com>
2020-09-21 19:48:44 +0530

    headerhomepage edited and made clickable

--- lib/homepage_widget/header_home_page.dart
+++ lib/homepage_widget/header_home_page.dart
@@ -1,25 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/theme/colors.dart';
 
-class HeaderHomePage extends StatelessWidget {
+class HeaderHomePage extends StatefulWidget {
   const HeaderHomePage({
     Key key,
   }) : super(key: key);
+
+  @override
+  _HeaderHomePageState createState() => _HeaderHomePageState();
+}
+
+class _HeaderHomePageState extends State<HeaderHomePage> {
+  bool nearMepressed = false;
+  bool followingpresseed = true;
+  bool followpressed = false;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Text(
-          "Following",
-          style: TextStyle(
-            color: white.withOpacity(0.7),
-            fontSize: 16,
+        GestureDetector(
+          child: Text(
+            "NearMe",
+            style: TextStyle(
+              color: (nearMepressed) ? white : white.withOpacity(0.7),
+              fontSize: 16,
+            ),
           ),
+          onTap: () {
+            print("Nearme pressed");
+            setState(() {
+              followingpresseed = false;
+              nearMepressed = true;
+              followpressed = false;
+            });
+          },
         ),
         SizedBox(
-          width: 8,
+          width: 6,
         ),
         Text(
           "|",
@@ -29,12 +48,54 @@ class HeaderHomePage extends StatelessWidget {
           ),
         ),
         SizedBox(
-          width: 8,
+          width: 6,
+        ),
+        GestureDetector(
+          child: Text(
+            "Following",
+            style: TextStyle(
+              color: (followingpresseed) ? white : white.withOpacity(0.7),
+              fontSize: 16,
+            ),
+          ),
+          onTap: () {
+            print("Following pressed");
+            setState(() {
+              followingpresseed = true;
+              nearMepressed = false;
+              followpressed = false;
+            });
+          },
+        ),
+        SizedBox(
+          width: 6,
         ),
         Text(
-          "For You",
+          "|",
           style: TextStyle(
-              color: white, fontSize: 17, fontWeight: FontWeight.w500),
+            color: white.withOpacity(0.3),
+            fontSize: 17,
+          ),
+        ),
+        SizedBox(
+          width: 6,
+        ),
+        GestureDetector(
+          child: Text(
+            "Follow",
+            style: TextStyle(
+                color: (followpressed ? white : white.withOpacity(0.7)),
+                fontSize: 17,
+                fontWeight: FontWeight.w500),
+          ),
+          onTap: () {
+            print("Follow pressed");
+            setState(() {
+              followingpresseed = false;
+              nearMepressed = false;
+              followpressed = true;
+            });
+          },
         )
       ],
     );
